faba4c8f08092e10ebd9e54fb60503d36e646a60
andiwinata <andi.winata@hotmail.com>
2019-06-22 14:47:20 +1000

    Updates

---
+++ precache-manifest.7bf2fb12ff7f7b74a92b55351e1a6eb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0952c2015f47d2b1fa8232e406ab911",
+    "url": "/i4c-rhok-winter-2019/index.html"
+  },
+  {
+    "revision": "e683d327134549c849f3",
+    "url": "/i4c-rhok-winter-2019/static/css/main.d644875d.chunk.css"
+  },
+  {
+    "revision": "4402a9baaff498bf7de6",
+    "url": "/i4c-rhok-winter-2019/static/js/2.ce4872b9.chunk.js"
+  },
+  {
+    "revision": "e683d327134549c849f3",
+    "url": "/i4c-rhok-winter-2019/static/js/main.b382a998.chunk.js"
+  },
+  {
+    "revision": "ff2315751c89f00e9a38",
+    "url": "/i4c-rhok-winter-2019/static/js/runtime~main.deb8c7ec.js"
+  }
+]);
